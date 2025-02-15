0f4a85e715511365a9936f6233383f5e83e27905
Oleksandr Moskaliuk <olekspm@gmail.com>
2025-02-15 11:09:07 +0100

    configure build script in package.json for gh-pages

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,12 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
-  basePath: '/crm',
+  basePath: process.env.NODE_ENV === 'production' ? '/github-pages-demo' : '',
   output: 'export',
   reactStrictMode: true,
+  images: {
+    unoptimized: true, // Required for static export
+  },
 };
 
 export default nextConfig;
