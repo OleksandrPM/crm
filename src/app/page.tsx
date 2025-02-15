3cd00b4a54a90c745a56e1ecbc6ae235a454c472
Oleksandr Moskaliuk <olekspm@gmail.com>
2025-02-15 22:31:37 +0100

    Rewrite label component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,14 @@
+import StatusLabel, { Status } from '@/app/components/StatusLabel/StatusLabel';
+
 export default function Home() {
   return (
     <div>
       <main>
-        <h1>Home Page</h1>
+        <h1 className="text-xl text-lime-300">Home Page</h1>
+        <StatusLabel status={Status.Active}>Active</StatusLabel>
+        <StatusLabel status={Status.NotActive}>NotActive</StatusLabel>
+        <StatusLabel status={Status.Pending}>Pending</StatusLabel>
+        <StatusLabel status={Status.Suspended}>Suspended</StatusLabel>
       </main>
     </div>
   );
