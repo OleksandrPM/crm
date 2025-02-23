79a1465c02b1b7752cdfee42eaee579780bca1ef
Oleksandr Moskaliuk <olekspm@gmail.com>
2025-02-23 16:52:32 +0100

    Configure next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,6 @@
 import type { NextConfig } from 'next';
 
+/* Version for GH-pages */
 const nextConfig: NextConfig = {
   /* config options here */
   basePath: process.env.NODE_ENV === 'production' ? '/github-pages-demo' : '',
@@ -9,5 +10,8 @@ const nextConfig: NextConfig = {
     unoptimized: true, // Required for static export
   },
 };
+
+/* Base version */
+// const nextConfig: NextConfig = {};
 
 export default nextConfig;
