0dbb0dc978ccf16cc16875048d3d8943d59e224d
Oleksandr Moskaliuk <olekspm@gmail.com>
2025-02-23 16:50:06 +0100

    Add AddCompanyButton to page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,14 @@
-import StatusLabel, { Status } from '@/app/components/StatusLabel/StatusLabel';
+// import { StatusLabel, Status } from './components/StatusLabel';
+import { headers } from 'next/headers';
+import AddCompanyButton from '@/app/components/AddCompanyButton';
 
 export default function Home() {
+  console.log(typeof headers());
+
   return (
-    <div>
-      <main>
-        <h1 className="text-xl text-lime-300">Home Page</h1>
-        <StatusLabel status={Status.Active}>Active</StatusLabel>
-        <StatusLabel status={Status.NotActive}>NotActive</StatusLabel>
-        <StatusLabel status={Status.Pending}>Pending</StatusLabel>
-        <StatusLabel status={Status.Suspended}>Suspended</StatusLabel>
-      </main>
-    </div>
+    <main>
+      <h1 className="text-4xl">Home Page</h1>
+      <AddCompanyButton />
+    </main>
   );
 }
