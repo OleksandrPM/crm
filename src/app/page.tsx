f02bc496e4c1d0bb3dfda69057302fbf26a7d365
Oleksandr Moskaliuk <olekspm@gmail.com>
2025-02-23 16:59:11 +0100

    Delete header`s logging in page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,7 @@
 // import { StatusLabel, Status } from './components/StatusLabel';
-import { headers } from 'next/headers';
 import AddCompanyButton from '@/app/components/AddCompanyButton';
 
 export default function Home() {
-  console.log(typeof headers());
-
   return (
     <main>
       <h1 className="text-4xl">Home Page</h1>
