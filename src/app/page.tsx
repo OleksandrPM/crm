4f5ef665e14eaac0c9b0359860100b12172a1858
Oleksandr Moskaliuk <olekspm@gmail.com>
2025-02-24 11:56:58 +0100

    Logging NODE_ENV

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,8 @@
 import AddCompanyButton from './components/AddCompanyButton';
 
 export default function Home() {
+  console.log(process.env.NODE_ENV);
+
   return (
     <main>
       <h1 className="text-4xl">Home Page</h1>
