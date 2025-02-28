d1706421d39887ed8c772ebb350b166f7fefef7c
Oleksandr Moskaliuk <olekspm@gmail.com>
2025-02-28 11:57:12 +0100

    Optimize the next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,18 +2,16 @@ import type { NextConfig } from 'next';
 
 const isProd = process.env.NODE_ENV === 'production';
 
-/* Version for GH-pages */
-const productionConfig: NextConfig = {
-  output: 'export',
-  reactStrictMode: true,
-  images: {
+const nextConfig: NextConfig = {};
+
+/* Updating for GH-pages */
+if (isProd) {
+  nextConfig.output = 'export';
+  nextConfig.reactStrictMode = true;
+  nextConfig.basePath = '/crm';
+  nextConfig.images = {
     unoptimized: true, // Required for static export
-  },
-};
-
-/* Base version */
-const baseConfig: NextConfig = {};
-
-const nextConfig: NextConfig = isProd ? productionConfig : baseConfig;
+  };
+}
 
 export default nextConfig;
