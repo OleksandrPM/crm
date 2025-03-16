877909436f132b85d83fe33d7ea044891013e48a
Oleksandr Moskaliuk <olekspm@gmail.com>
2025-03-16 22:31:51 +0100

    Add notFound handler

--- src/app/(admin)/companies/[id]/page.tsx
+++ src/app/(admin)/companies/[id]/page.tsx
@@ -1,14 +1,28 @@
-import React from 'react';
+'use client';
+
+import React, { useEffect } from 'react';
 import Header from '@/app/components/Header';
+import { notFound } from 'next/navigation';
 
 export interface PageProps {
-  params: { id: string };
+  params: Promise<{ id: string }>;
 }
 
 export default function Page({ params }: PageProps) {
+  const { id } = React.use(params);
+
+  useEffect(() => {
+    const idNumber = Number.parseInt(id);
+
+    if (Number.isNaN(idNumber)) {
+      // Redirect or handle invalid ID
+      notFound();
+    }
+  }, [id]);
+
   return (
     <>
-      <Header>Company ({params.id})</Header>
+      <Header>Company ({id})</Header>
     </>
   );
 }
