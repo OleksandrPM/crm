2a8ac5097d32fe018142e51bf76d658461bf703e
Oleksandr Moskaliuk <olekspm@gmail.com>
2025-03-23 12:26:08 +0100

    Fix props error in companies/[id]/page.tsx

--- src/app/(admin)/companies/[id]/page.tsx
+++ src/app/(admin)/companies/[id]/page.tsx
@@ -1,28 +1,11 @@
-'use client';
+import React from 'react';
 
-import React, { useEffect } from 'react';
 import Header from '@/app/components/Header';
-import { notFound } from 'next/navigation';
 
 export interface PageProps {
-  params: Promise<{ id: string }>;
+  params: { id: string };
 }
 
 export default function Page({ params }: PageProps) {
-  const { id } = React.use(params);
-
-  useEffect(() => {
-    const idNumber = Number.parseInt(id);
-
-    if (Number.isNaN(idNumber)) {
-      // Redirect or handle invalid ID
-      notFound();
-    }
-  }, [id]);
-
-  return (
-    <>
-      <Header>Company ({id})</Header>
-    </>
-  );
+  return <Header>{`Company (${params.id})`}</Header>;
 }
