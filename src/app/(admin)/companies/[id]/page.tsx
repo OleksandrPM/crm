1d3dd2a2d231e85692fd39e7b8f5c2d7d69c42df
Oleksandr Moskaliuk <olekspm@gmail.com>
2025-03-23 12:37:19 +0100

    Fix pages

--- src/app/(admin)/companies/[id]/page.tsx
+++ src/app/(admin)/companies/[id]/page.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 
-import Header from '@/app/components/Header';
-
 export interface PageProps {
   params: { id: string };
 }
 
 export default function Page({ params }: PageProps) {
-  return <Header>{`Company (${params.id})`}</Header>;
+  return (
+    <div className="py-6 px-10">
+      <p>{`Information about company (${params.id})`}</p>
+    </div>
+  );
 }
