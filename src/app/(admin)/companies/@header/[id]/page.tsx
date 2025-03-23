b8148b07db4d492ab917ebe7dc566538d7656800
Oleksandr Moskaliuk <olekspm@gmail.com>
2025-03-23 13:40:37 +0100

    Set pages props with params

--- src/app/(admin)/companies/@header/[id]/page.tsx
+++ src/app/(admin)/companies/@header/[id]/page.tsx
@@ -1,10 +1,35 @@
-import React from 'react';
+'use client';
+
+import React, { useEffect, useState } from 'react';
 import Header from '@/app/components/Header';
+import { notFound } from 'next/navigation';
 
 export interface PageProps {
-  params: { id: string };
+  params: Promise<{ id: string }>;
 }
 
 export default function Page({ params }: PageProps) {
-  return <Header>{`Company (${params.id})`}</Header>;
+  const [id, setId] = useState<string | null>(null);
+
+  useEffect(() => {
+    params
+      .then(({ id }) => {
+        const idNumber = Number.parseInt(id);
+
+        if (Number.isNaN(idNumber)) {
+          // Redirect or handle invalid ID
+          notFound();
+        } else {
+          setId(id);
+        }
+      })
+      .catch(() => {
+        notFound(); // Handle promise rejection
+      });
+  }, [params]);
+
+  // Render nothing until 'id' is resolved
+  if (!id) return null;
+
+  return <Header>{`Company (${id})`}</Header>;
 }
--- src/app/(admin)/companies/[id]/page.tsx
+++ src/app/(admin)/companies/[id]/page.tsx
@@ -1,13 +1,38 @@
-import React from 'react';
+'use client';
+
+import React, { useEffect, useState } from 'react';
+import { notFound } from 'next/navigation';
 
 export interface PageProps {
-  params: { id: string };
+  params: Promise<{ id: string }>;
 }
 
 export default function Page({ params }: PageProps) {
+  const [id, setId] = useState<string | null>(null);
+
+  useEffect(() => {
+    params
+      .then(({ id }) => {
+        const idNumber = Number.parseInt(id);
+
+        if (Number.isNaN(idNumber)) {
+          // Redirect or handle invalid ID
+          notFound();
+        } else {
+          setId(id);
+        }
+      })
+      .catch(() => {
+        notFound(); // Handle promise rejection
+      });
+  }, [params]);
+
+  // Render nothing until 'id' is resolved
+  if (!id) return null;
+
   return (
     <div className="py-6 px-10">
-      <p>{`Information about company (${params.id})`}</p>
+      <p>{`Information about company (${id})`}</p>
     </div>
   );
 }
